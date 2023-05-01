95e2d6fc890ae9cb32af3397fe9396ab71b87c23
Abhijeet <abhijeetgautam572@gmail.com>
2023-05-01 14:57:51 +0530

    UI changes to input text-field

--- lib/pallete.dart
+++ lib/pallete.dart
@@ -10,4 +10,10 @@ class Pallete {
   static const Color borderColor = Color.fromRGBO(200, 200, 200, 1);
   static const Color blackColor = Colors.black;
   static const Color whiteColor = Colors.white;
+  static const Color textFieldColor = Color.fromRGBO(52, 53, 65, 1);
+  static const Color textBoxColor = Color.fromRGBO(65, 65, 78, 1);
+  static const Color primaryColor = Color.fromRGBO(16, 163, 127, 1);
 }
+
+//rgb(52,53,65)
+//rgb(16,163,127)
--- lib/test.dart
+++ lib/test.dart
@@ -24,74 +24,126 @@ class _TestPromptState extends State<TestPrompt> {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          // Chat Bubble
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Container(
-              margin: const EdgeInsets.symmetric(
-                horizontal: 15,
-              ),
-              padding: const EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Pallete.borderColor,
-                ),
-                borderRadius: BorderRadius.circular(20).copyWith(
-                  topLeft: Radius.zero,
-                ),
-              ),
-              child: Text(
-                chatSpeech == "" ? "Ask me" : chatSpeech,
-              ),
-            ),
-          ),
-          // Input Text-field
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Container(
-                  width: MediaQuery.of(context).size.width * 0.85,
-                  padding: EdgeInsets.all(8),
-                  child: TextField(
-                    controller: promptController,
-                    autocorrect: true,
-                    autofocus: true,
-                    maxLength: 200,
-                    maxLines: null,
-                    keyboardType: TextInputType.multiline,
-                    decoration: const InputDecoration(
-                      hintText: 'Give me a prompt...',
+    return Stack(
+      children: [
+        Column(
+          children: [
+            // Chat Bubble
+            SingleChildScrollView(
+              child: Container(
+                height: MediaQuery.of(context).size.height * 0.8988,
+                child: Align(
+                  alignment: Alignment.topLeft,
+                  child: Container(
+                    margin: const EdgeInsets.symmetric(
+                      horizontal: 15,
+                    ),
+                    padding: const EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Pallete.borderColor,
+                      ),
+                      borderRadius: BorderRadius.circular(20).copyWith(
+                        topLeft: Radius.zero,
+                      ),
+                    ),
+                    child: Text(
+                      chatSpeech == "" ? "Ask me" : chatSpeech,
                     ),
                   ),
                 ),
-                IconButton(
-                  onPressed: () async {
-                    setState(
-                      () {
-                        prompt = promptController.value.text.toString();
-                      },
-                    );
-                    final speech = await openAiService.isArtImagePrompt(prompt);
-                    setState(() {
-                      chatSpeech = speech;
-                    });
-                    // print(speech);
-                    promptController.clear();
-                  },
-                  icon: const Icon(
-                    Icons.send,
+              ),
+            ),
+          ],
+        ),
+        //bottom section
+        Positioned(
+          bottom: 0,
+          left: 0,
+          right: 0,
+          child: // Input Text-field
+              Container(
+            // height: MediaQuery.of(context).size.height * 0.1,
+            decoration: const BoxDecoration(
+              color: Pallete.textFieldColor,
+              // border: Border.all(
+              //   color: Colors.red,
+              // ),
+            ),
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  //Input text field
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.9,
+                    padding: EdgeInsets.all(8),
+                    child: TextField(
+                      cursorColor: Colors.grey,
+                      cursorHeight: 30,
+                      controller: promptController,
+                      autocorrect: true,
+                      autofocus: true,
+                      // maxLength: 200,
+
+                      maxLines: null,
+                      keyboardType: TextInputType.multiline,
+                      style: const TextStyle(
+                        color: Colors.white,
+                      ),
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(7),
+                          borderSide: BorderSide.none,
+                        ),
+                        filled: true,
+                        fillColor: Pallete.textBoxColor,
+                        hintStyle: const TextStyle(
+                          color: Colors.grey,
+                        ),
+                        hintText: 'Give me a prompt...',
+                      ),
+                    ),
                   ),
-                ),
-              ],
+
+                  // send button
+                  Container(
+                    // decoration: BoxDecoration(
+                    //   border: Border.all(
+                    //     color: Colors.red,
+                    //   ),
+                    // ),
+                    child: IconButton(
+                      onPressed: () async {
+                        setState(
+                          () {
+                            prompt = promptController.value.text.toString();
+                          },
+                        );
+                        final speech =
+                            await openAiService.isArtImagePrompt(prompt);
+                        setState(() {
+                          chatSpeech = speech;
+                        });
+                        // print(speech);
+                        promptController.clear();
+                      },
+                      icon: const Icon(
+                        Icons.send,
+                        color: Pallete.primaryColor,
+                        size: 26,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
-        ],
-      ),
+        )
+      ],
     );
   }
 }
